0ca70848235cd584f387e277680c1c0d5b2b0fe4
AlexZyryanov72 <comwek80@gmail.com>
2020-03-12 00:41:45 +0500

    setting

--- ART/settings.py
+++ ART/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!9sm)_w$i5230&1f#n2zl8l7ez^plvsf4-17^b1h2-f==*0jb-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
